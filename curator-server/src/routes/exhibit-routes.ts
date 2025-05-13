2b4044e7760ac545018094753b303cf814e936bd
Wilson Stewart <wilson@bellbellbell.com>
2025-05-12 22:45:02 -0600

    refactor: updated import names

--- curator-server/src/routes/exhibit-routes.ts
+++ curator-server/src/routes/exhibit-routes.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { db } from "..";
 import { exhibits } from "../db/schema";
 
-const exhibitRoutes = new Hono()
+export const exhibitRoutes = new Hono();
 
 exhibitRoutes.get("/", async (c) => {
   return c.json(await db.select().from(exhibits));
