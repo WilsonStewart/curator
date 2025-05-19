fa940123042b1e7e73deeabdbaa754c5f38605b6
William Wilson Stewart <ww@bellbellbell.com>
2025-05-19 01:07:10 -0600

    adjusted schema and seed data to include new id eid paradigm, as well as adding explicit column names in schema.ts

--- curator-server/src/db/common-columns.ts
+++ curator-server/src/db/common-columns.ts
@@ -1,17 +1,24 @@
-import { timestamp, uuid } from "drizzle-orm/pg-core";
+import { bigint, integer, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { museums, users } from "./schema";
 
+export const identityColumns = {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  eid: text("eid").notNull().unique(),
+};
+
 export const timestampColumns = {
-  createdAt: timestamp().defaultNow().notNull(),
-  modifiedAt: timestamp().defaultNow().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  modifiedAt: timestamp("modified_at").notNull().defaultNow(),
 };
 
 export const ownerColumns = {
-    createdBy: uuid().notNull().references(() => users.id),
-}
+  createdBy: integer("created_by")
+    .notNull()
+    .references(() => users.id),
+};
 
 export const museumColumns = {
-      museumId: uuid()
-        .notNull()
-        .references(() => museums.id),
-}
+  museumId: integer("museum_id")
+    .notNull()
+    .references(() => museums.id),
+};
