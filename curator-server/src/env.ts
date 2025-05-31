256bde483c9a4a65a1e8b771106d557a5cf9b441
William Wilson Stewart <ww@bellbellbell.com>
2025-05-31 12:56:07 -0600

    multi-feat: added basic logger, refactored figlet banner, added error handler

--- curator-server/src/env.ts
+++ curator-server/src/env.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import "dotenv/config";
 
 const ZEnvSchema = z.object({
   NODE_ENV: z.enum(["production", "development"]),
@@ -7,4 +8,15 @@ const ZEnvSchema = z.object({
   DATABASE_URL: z.string().url(),
 });
 
+// eslint-disable-next-line ts/no-redeclare
+const { data: env, error } = ZEnvSchema.safeParse(process.env);
+
+if (error) {
+  console.error("❌ Invalid env:");
+  console.error(JSON.stringify(error.flatten().fieldErrors, null, 2));
+  process.exit(1);
+}
+
 export type TEnvVars = z.infer<typeof ZEnvSchema>;
+
+export default env!;
--- curator-server/src/index.ts
+++ curator-server/src/index.ts
@@ -1,14 +1,44 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { api } from "@/api";
+import env from "@/env";
+import pkgdotjson from "@/../package.json" assert { type: "json" };
 
-export const db = drizzle(process.env.DATABASE_URL!);
+// Generated at https://patorjk.com/software/taag/#p=display&f=Bloody&t=Curator%20Api
+const apiFiglet = `
+ ▄████▄   █    ██  ██▀███   ▄▄▄     ▄▄▄█████▓ ▒█████   ██▀███      ▄▄▄       ██▓███   ██▓
+▒██▀ ▀█   ██  ▓██▒▓██ ▒ ██▒▒████▄   ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒   ▒████▄    ▓██░  ██▒▓██▒
+▒▓█    ▄ ▓██  ▒██░▓██ ░▄█ ▒▒██  ▀█▄ ▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒   ▒██  ▀█▄  ▓██░ ██▓▒▒██▒
+▒▓▓▄ ▄██▒▓▓█  ░██░▒██▀▀█▄  ░██▄▄▄▄██░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄     ░██▄▄▄▄██ ▒██▄█▓▒ ▒░██░
+▒ ▓███▀ ░▒▒█████▓ ░██▓ ▒██▒ ▓█   ▓██▒ ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒    ▓█   ▓██▒▒██▒ ░  ░░██░
+░ ░▒ ▒  ░░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░    ▒▒   ▓▒█░▒▓▒░ ░  ░░▓  
+  ░  ▒   ░░▒░ ░ ░   ░▒ ░ ▒░  ▒   ▒▒ ░   ░      ░ ▒ ▒░   ░▒ ░ ▒░     ▒   ▒▒ ░░▒ ░      ▒ ░
+░         ░░░ ░ ░   ░░   ░   ░   ▒    ░      ░ ░ ░ ▒    ░░   ░      ░   ▒   ░░        ▒ ░
+░ ░         ░        ░           ░  ░            ░ ░     ░              ░  ░          ░  
+░                                                                                        
+`;
+
+const printFiglet = () => {
+  switch (env.CURATOR_MODE) {
+    case "api":
+      console.log(apiFiglet);
+  }
+  console.log(`Started at:     ${new Date()}`);
+  console.log(`Package Name:   ${pkgdotjson.name}`);
+  console.log(`Version:        ${pkgdotjson.version}`);
+  console.log(`NODE_ENV:       ${env.NODE_ENV}`);
+  console.log(`CURATOR_MODE:   ${env.CURATOR_MODE}`);
+  console.log("");
+};
+
+export const db = drizzle(env.DATABASE_URL!);
 
 const start = () => {
-  switch (process.env.CURATOR_MODE) {
+  printFiglet();
+  switch (env.CURATOR_MODE) {
     case "api":
       return {
-        port: process.env.PORT,
+        port: env.SERVER_PORT,
         fetch: api.fetch,
       };
   }
