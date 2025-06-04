b31f6d7cd4a33a2f50fa47bb2d669d271e45c54a
William Wilson Stewart <ww@bellbellbell.com>
2025-06-04 09:43:15 -0600

    Added db check query

--- curator-server/src/index.ts
+++ curator-server/src/index.ts
@@ -3,11 +3,21 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { api } from "@/api";
 import env from "@/env";
 import { printBanner } from "@/lib/banner";
+import { metadata } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export const db = drizzle(env.DATABASE_URL);
 
-const start = () => {
+const start = async () => {
   printBanner();
+
+  // Test db connection, kill if not up
+  try { await db.select().from(metadata).where(eq(metadata.id, "curatorMetadata")) } catch (error) {
+    console.error("🛜 Database check query failed!")
+    console.error(error)
+    process.exit(1)
+  }
+
   switch (env.CURATOR_MODE) {
     case "api":
       return {
