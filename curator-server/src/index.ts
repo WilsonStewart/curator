f212d171eed5eb743f7a859c3e995f03b572163a
William Wilson Stewart <ww@bellbellbell.com>
2025-06-03 22:28:36 -0600

    feat/refactor: extracted banner to external function, added git info and git facts, added new figlets for other curator modes

--- curator-server/src/index.ts
+++ curator-server/src/index.ts
@@ -2,32 +2,12 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { api } from "@/api";
 import env from "@/env";
-import pkgdotjson from "@/../package.json" assert { type: "json" };
-import { apiFiglet } from "@/lib/figlets";
-
-const printFiglet = () => {
-  switch (env.CURATOR_MODE) {
-    case "api":
-      console.log(apiFiglet);
-  }
-  console.log(`Started at:         ${new Date()}`);
-  console.log(`Package name:       ${pkgdotjson.name}`);
-  console.log(`Version:            ${pkgdotjson.version}`);
-  console.log(`NODE_ENV:           ${env.NODE_ENV}`);
-  console.log(`CURATOR_MODE:       ${env.CURATOR_MODE}`);
-  if (env.CURATOR_MODE === "api") {
-    console.log(
-      `OpenAPI endpoint:   http://localhost:${env.SERVER_PORT}/openapi.json`
-    );
-    console.log(`Scaler endpoint:    http://localhost:${env.SERVER_PORT}/docs`);
-  }
-  console.log("");
-};
+import { printBanner } from "@/lib/banner";
 
 export const db = drizzle(env.DATABASE_URL);
 
 const start = () => {
-  printFiglet();
+  printBanner();
   switch (env.CURATOR_MODE) {
     case "api":
       return {
