70455c74ec90fae2efc1e527d626d220ceff6b52
Wilson Stewart <wilson@bellbellbell.com>
2025-06-16 18:48:47 -0600

    fixed login page format

--- curator-web/src/components/login-form.tsx
+++ curator-web/src/components/login-form.tsx
@@ -1,14 +1,22 @@
-import { SocialButton } from "@/components/social-button"
+import { SocialButton } from "@/components/social-button";
+import { KeyIcon } from "@phosphor-icons/react";
 
 export const LoginForm = () => {
-    return (
-        <div className="login">
-            <img src="/bone.png"></img>
-            <h1 className="login-title">login to <span>curator</span></h1>
-            <input placeholder="email" />
-            <input placeholder="password" type="password" />
-            <button>Login with username and password</button>
-            <SocialButton />
-        </div>
-    )
-}
+  return (
+    <div className="login">
+      <img src="/bone.png"></img>
+      <h1 className="login-title">
+        login to <span>curator</span>
+      </h1>
+      <input placeholder="email" />
+      <input placeholder="password" type="password" />
+      <div className="login--buttons">
+        <button>
+          {/* <KeyIcon color="gray" size={30} style={{ marginRight: "10px" }} /> */}
+          Login
+        </button>
+        {/* <SocialButton /> */}
+      </div>
+    </div>
+  );
+};
