9c530f080bfcc93b27223eb5a1fd7fa302d9531d
William Wilson Stewart <ww@bellbellbell.com>
2025-06-24 16:18:14 -0600

    added aliases

--- curator-server/src/logic/exhibits/L.exhibits.select-many.ts
+++ curator-server/src/logic/exhibits/L.exhibits.select-many.ts
@@ -1,37 +1,26 @@
 import { db } from "@/lib/db";
-import { ETDYoutubeChannels, ETDYoutubeVideos, exhibits } from "@/schemas/drizzle-schema/drizzle-schema.exhibits";
+import { exhibitsData, exhibitsTypeData_YoutubeChannels, exhibitsTypeData_YoutubeVideos } from "@/lib/table-aliases";
 import { VExhibitsSelectManyQuery } from "@/schemas/validator-schema/v-exhibits";
 import { eq, like } from "drizzle-orm";
-import { alias } from "drizzle-orm/pg-core";
 import { z } from "zod";
 
 export const LExhibitsSelectMany = async (query: z.infer<typeof VExhibitsSelectManyQuery>) => {
-  console.log(query)
-
-  const data = alias(exhibits, "data")
-  const typeData_YoutubeChannels = alias(ETDYoutubeChannels, "typeData_YoutubeChannels")
-  const typeData_YoutubeVideos = alias(ETDYoutubeVideos, "typeData_YoutubeVideos")
-
   let q = db
     .select()
-    .from(data)
-    .leftJoin(typeData_YoutubeChannels, eq(data.id, typeData_YoutubeChannels.exhibitId))
-    .leftJoin(typeData_YoutubeVideos, eq(data.id, typeData_YoutubeVideos.exhibitId))
+    .from(exhibitsData)
+    .leftJoin(exhibitsTypeData_YoutubeChannels, eq(exhibitsData.id, exhibitsTypeData_YoutubeChannels.exhibitId))
+    .leftJoin(exhibitsTypeData_YoutubeVideos, eq(exhibitsData.id, exhibitsTypeData_YoutubeVideos.exhibitId))
 
-  if (Object.keys(query).length > 0) {
-    Object.keys(query).forEach(k => {
-      switch (k) {
-        case "nameLike": {
-          q.where(like(data.name, `%${query[k]}%`))
-        }
-        case "youtubeChannelHandle": {
-          q.where(like(typeData_YoutubeChannels.youtubeChannelHandle, `%${query[k]}%`))
-        }
+  Object.keys(query).forEach(k => {
+    switch (k) {
+      case "nameLike": {
+        q.where(like(exhibitsData.name, `%${query[k]}%`))
       }
-    });
-  }
-
-  console.log(q)
+      case "youtubeChannelHandle": {
+        q.where(like(exhibitsTypeData_YoutubeChannels.youtubeChannelHandle, `%${query[k]}%`))
+      }
+    }
+  });
 
   let r = await q.execute()
 
