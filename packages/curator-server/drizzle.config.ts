1e4f78ce8686db6f11c0b4d8ebb24c1b77cbf461
Wilson Stewart <wilson@bellbellbell.com>
2026-01-06 08:53:16 +0000

    chore: wiped out old bad drizzle migrations

--- packages/curator-server/drizzle.config.ts
+++ packages/curator-server/drizzle.config.ts
@@ -2,11 +2,13 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./drizzle",
-  schema: "./src/schemas/drizzle-schema",
-  dialect: "postgresql",
-  casing: "snake_case",
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
+	out: "./drizzle",
+	schema: "./src/schemas/drizzle-schema",
+	dialect: "postgresql",
+	casing: "snake_case",
+	dbCredentials: {
+		url:
+			process.env.DATABASE_URL ||
+			"postgres://postgres:1234@localhost:5432/postgres",
+	},
 });
